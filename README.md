12de3952543aa81db98503f15d9a288dcf337e43
EricP <eric.paque@outlook.be>
2023-09-16 01:12:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#OneDrive auto migration
+
+This is a simple script to migrate files between OneDrive accounts.
+
+## Configuration
+The only things you need to configure is the "config.json" which takes 4 parameters:
+
+- "source": The source folder where the OneDrive(1) folder is located
+- "destination": The destination folder where the OneDrive(2) folder is located
+- "copy_existing": Whether to copy (True) or not (False) existing files in the destination folder
+- "exception" (optional): A list of files to ignore
+
+Example:
+```json
+{
+    "source": "C:\\Users\\user1\\OneDrive",
+    "destination": "C:\\Users\\user2\\OneDrive",
+    "copy_existing": false,
+    "exception": [
+        "README.md"
+    ]
+}
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+import subprocess
+import shutil
+import json
+def is_it_exception(path, exception):
+    for e in exception:
+        if e in path:
+            return True
+    return False
+def switch_drive(source, destination,copy_existing, exception):
+    # Use a breakpoint in the code line below to debug your script.
+    # Iterate through directory
+    for root, dirs, files in os.walk(source):
+        for name in files:
+
+            path = os.path.join(root, name)
+            path_modified = path.replace(source, destination)
+            if copy_existing == False:
+                if os.path.exists(path_modified):
+                    continue
+            dir_name = os.path.dirname(path_modified)
+            try:
+                os.makedirs(dir_name, exist_ok=True)
+                shutil.copy(path, path_modified)
+
+                if not is_it_exception(dir_name, exception):
+                    subprocess.run('attrib +U -P "' + path + '"')
+                subprocess.run('attrib +U -P "' + path_modified + '"')
+            except Exception as e:
+                with open("exception.txt", "a", encoding="utf-8") as f:
+                    f.write("Exception au fichier : "+path+"\n")
+def read_config():
+    f = open("config.json", "r", encoding="utf-8")
+    file = json.load(f)
+    source = file["source"]
+    destination = file["destination"]
+    copy_existing = file["copy_existing"]
+    exception = file["exception"]
+
+    return source, destination,copy_existing, exception
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    source, dest,copy_existing, exception = read_config()
+    switch_drive(source, dest,copy_existing, exception)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
