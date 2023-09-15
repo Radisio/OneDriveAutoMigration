3d6f88edaad5b7ac952306a8dde3e68c639e6a81
EricP <eric.paque@outlook.be>
2023-09-16 01:20:24 +0200

    Add features: Migration OneDrive to local storage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #OneDrive auto migration
 
-This is a simple script to migrate files between OneDrive accounts.
+This is a simple script to migrate files between OneDrive accounts or only to save OneDrive files/folders into local storage.
 
 ## Configuration
 The only things you need to configure is the "config.json" which takes 4 parameters:
@@ -8,6 +8,7 @@ The only things you need to configure is the "config.json" which takes 4 paramet
 - "source": The source folder where the OneDrive(1) folder is located
 - "destination": The destination folder where the OneDrive(2) folder is located
 - "copy_existing": Whether to copy (True) or not (False) existing files in the destination folder
+- "destination_onedrive": Whether the destination folder is a OneDrive folder (True) or not (False)
 - "exception" (optional): A list of files to ignore
 
 Example:
@@ -16,6 +17,7 @@ Example:
     "source": "C:\\Users\\user1\\OneDrive",
     "destination": "C:\\Users\\user2\\OneDrive",
     "copy_existing": false,
+    "destination_onedrive": true,
     "exception": [
         "README.md"
     ]
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def is_it_exception(path, exception):
         if e in path:
             return True
     return False
-def switch_drive(source, destination,copy_existing, exception):
+def switch_drive(source, destination,copy_existing, destination_onedrive,exception):
     # Use a breakpoint in the code line below to debug your script.
     # Iterate through directory
     for root, dirs, files in os.walk(source):
@@ -20,7 +20,7 @@ def switch_drive(source, destination,copy_existing, exception):
 
             path = os.path.join(root, name)
             path_modified = path.replace(source, destination)
-            if copy_existing == False:
+            if not copy_existing:
                 if os.path.exists(path_modified):
                     continue
             dir_name = os.path.dirname(path_modified)
@@ -30,7 +30,8 @@ def switch_drive(source, destination,copy_existing, exception):
 
                 if not is_it_exception(dir_name, exception):
                     subprocess.run('attrib +U -P "' + path + '"')
-                subprocess.run('attrib +U -P "' + path_modified + '"')
+                if destination_onedrive:
+                    subprocess.run('attrib +U -P "' + path_modified + '"')
             except Exception as e:
                 with open("exception.txt", "a", encoding="utf-8") as f:
                     f.write("Exception au fichier : "+path+"\n")
@@ -40,12 +41,13 @@ def read_config():
     source = file["source"]
     destination = file["destination"]
     copy_existing = file["copy_existing"]
+    destination_onedrive = file["destination_onedrive"]
     exception = file["exception"]
 
-    return source, destination,copy_existing, exception
+    return source, destination,copy_existing, destination_onedrive,exception
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    source, dest,copy_existing, exception = read_config()
-    switch_drive(source, dest,copy_existing, exception)
+    source, dest,copy_existing,destination_onedrive,exception = read_config()
+    switch_drive(source, dest,copy_existing,destination_onedrive, exception)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
